771e2322841d6acef92f2562dc2299a1b4c7e393
Dida1209 <duhuiying1209@Outlook.com>
2017-02-16 22:55:23 +0800

    luyou

--- config/routes.js
+++ config/routes.js
@@ -27,27 +27,48 @@ module.exports=function(app) {
 //comment
     app.post('/user/comment',user.ifSignin,comment.save);
 
-//各种资源点击时(视频、文档、在线测试)
-    app.get('/resource', function (req, res) {
-        res.render('resource', {});
-    });
+//搜索供功能
 
-//视频页+学习目录
-    app.get('/video', function (req, res) {
-        res.render('video', {});
-    });
-//文档页+学习目录
+//各种资源列表(视频、文档、在线测试)
+    app.get('/videolist',function(req,res){
+
+    })
+
+    app.get('/doclist',function(req,res){
+
+    })
+
+    app.get('/testlist',function(req,res){
+
+    })
+//各种资源被点击展示时(视频、文档、在线测试)
+    app.get('/video',function(req,res){
+        res.render('video',{})
+    })
+
     app.get('/doc', function (req, res) {
         res.render('document', {});
     });
 
-//在线测试+学习目录
     app.get('/test', function (req, res) {
+        res.render('onlineTest', {});
+    });
+//视频页+学习目录
+    app.get('/video/:subjection', function (req, res) {
+        res.render('video', {});
+    });
+//文档页+学习目录
+    app.get('/doc/:subjection', function (req, res) {
+        res.render('document', {});
+    });
+
+//在线测试+学习目录
+    app.get('/test/:subjection', function (req, res) {
         res.render('onlineTest', {});
     });
 
 //概念页(只有学习目录各个知识点被点击时)
-    app.get('/bg', function (req, res) {
+    app.get('/kd/:subjection', function (req, res) {
         res.render('knowledgeSystem', {});
     });
 
